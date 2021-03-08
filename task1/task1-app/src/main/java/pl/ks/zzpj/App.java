17151344d24331f71afe6dfd9e0441f457d1eaab
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-03-08 13:08:00 +0100

    Application using lib api added

---
+++ task1/task1-app/src/main/java/pl/ks/zzpj/App.java
@@ -0,0 +1,14 @@
+package pl.ks.zzpj;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person person = new Person("Adam");
+        System.out.println( "Hello " + person.getName() );
+    }
+}
