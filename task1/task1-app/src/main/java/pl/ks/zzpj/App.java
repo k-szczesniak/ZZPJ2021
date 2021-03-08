a17af79e3ea6b539c7c235d893e7973478915597
Krzysztof Szczesniak <krzys.szcze@gmail.com>
2021-03-08 14:46:10 +0100

    dev and prod properties added

--- task1/task1-app/src/main/java/pl/ks/zzpj/App.java
+++ task1/task1-app/src/main/java/pl/ks/zzpj/App.java
@@ -1,5 +1,9 @@
 package pl.ks.zzpj;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
@@ -8,7 +12,20 @@ public class App
 {
     public static void main( String[] args )
     {
-        Person person = new Person("Adam");
+        Properties prop = loadPropertiesFile("config.properties");
+        Person person = new Person(prop.getProperty("personName"));
         System.out.println( person.introducePerson() );
+    }
+
+    public static Properties loadPropertiesFile(String filePath) {
+
+        Properties prop = new Properties();
+
+        try (InputStream resourceAsStream = App.class.getClassLoader().getResourceAsStream(filePath)) {
+            prop.load(resourceAsStream);
+        } catch (IOException e) {
+            System.err.println("Unable to load properties file : " + filePath);
+        }
+        return prop;
     }
 }
